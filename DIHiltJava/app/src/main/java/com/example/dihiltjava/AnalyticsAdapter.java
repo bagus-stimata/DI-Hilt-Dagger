b6754bb5e6332f7ce260373554d42698197ab3a2
bagus-stimata <bagus.stimata@gmail.com>
2020-11-03 19:16:23 +0700

    oke bos

---
+++ DIHiltJava/app/src/main/java/com/example/dihiltjava/AnalyticsAdapter.java
@@ -0,0 +1,9 @@
+package com.example.dihiltjava;
+
+import android.util.Log;
+
+public class AnalyticsAdapter {
+    public void hello(){
+        Log.d("hello", "Ini tag hello");
+    }
+}
---
+++ DIHiltJava/app/src/main/java/com/example/dihiltjava/ExampleApplication.java
@@ -0,0 +1,9 @@
+package com.example.dihiltjava;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class ExampleApplication extends Application {
+}
